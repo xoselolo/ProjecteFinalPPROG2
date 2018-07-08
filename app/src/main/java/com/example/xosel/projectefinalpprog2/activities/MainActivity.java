e04645e7421af457e95f40adf26bdcba1c6d5b75
xoselolo <1a09lopxose@gmail.com>
2018-07-08 15:48:35 +0200

    adapter

--- app/src/main/java/com/example/xosel/projectefinalpprog2/activities/MainActivity.java
+++ app/src/main/java/com/example/xosel/projectefinalpprog2/activities/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.xosel.projectefinalpprog2.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.xosel.projectefinalpprog2.R;
 
@@ -11,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Fiquem la Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         //Hola
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main_activity, menu);
+        return true;
     }
 }
