cc67269f4b56ae170bfc0414201b8c71c8b2c0b7
xoselolo <1a09lopxose@gmail.com>
2018-07-08 13:11:18 +0200

    Escola i Tipus Escola afegits.

--- app/src/main/java/com/example/xosel/projectefinalpprog2/activities/MainActivity.java
+++ app/src/main/java/com/example/xosel/projectefinalpprog2/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.xosel.projectefinalpprog2;
+package com.example.xosel.projectefinalpprog2.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.xosel.projectefinalpprog2.R;
 
 public class MainActivity extends AppCompatActivity {
 
