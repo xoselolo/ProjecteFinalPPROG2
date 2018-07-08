1d69f62cb535660f811a1cdbbd26d487719606c4
oscargalles <oscar.galles.2015@salleurl.edu>
2018-07-08 16:21:00 +0200

    tabs and viewpager

--- app/src/main/java/com/example/xosel/projectefinalpprog2/activities/MainActivity.java
+++ app/src/main/java/com/example/xosel/projectefinalpprog2/activities/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.xosel.projectefinalpprog2.activities;
 
+import android.support.design.widget.TabLayout;
+
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.TableLayout;
 
 import com.example.xosel.projectefinalpprog2.R;
+import com.example.xosel.projectefinalpprog2.adapters.TabAdapter;
+import com.example.xosel.projectefinalpprog2.fragments.LlistaEscolesFragment;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +24,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Fiquem la Toolbar
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        createToolbar();
+        createTabs();
 
         //Hola
+    }
+
+
+
+    private void createToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void createTabs() {
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ArrayList<TabAdapter.TabEntry> entries = new ArrayList<>();
+        entries.add(new TabAdapter.TabEntry(new LlistaEscolesFragment(), "All"));
+        entries.add(new TabAdapter.TabEntry(new LlistaEscolesFragment(), "School"));
+        entries.add(new TabAdapter.TabEntry(new LlistaEscolesFragment(), "Other"));
+
+        TabAdapter adapter = new TabAdapter(getSupportFragmentManager(), entries);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
---
+++ app/src/main/java/com/example/xosel/projectefinalpprog2/adapters/TabAdapter.java
@@ -0,0 +1,51 @@
+package com.example.xosel.projectefinalpprog2.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class TabAdapter extends FragmentPagerAdapter{
+    public static class TabEntry {
+        private Fragment fragment;
+        private String name;
+
+        public TabEntry(Fragment fragment, String name) {
+            this.fragment = fragment;
+            this.name = name;
+        }
+
+        public Fragment getFragment() {
+            return fragment;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private List<TabEntry> entries;
+
+
+    public TabAdapter(FragmentManager fm, List<TabEntry> entries) {
+        super(fm);
+        this.entries = entries;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return entries.get(position).getFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return entries.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return entries.get(position).getName();
+    }
+}
+
